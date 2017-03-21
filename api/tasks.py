a544659babd8a23b8f982067262520ef971ca79e
shivji <jindalshivam65@gmail.com>
2017-03-21 10:51:35 +0530

    added app code

--- api/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-
-from api.models import Work
-
-
-@shared_task
-def pay():
-    Work.objects.filter(payment_done=False, verified_by__isnull=False).update(payment_done=True)
